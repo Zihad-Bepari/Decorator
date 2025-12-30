0c92af942edebeec30a3a48948becad62f68675c
Zihad-Bepari <2215151139@uits.edu.bd>
2025-12-31 05:20:52 +0600

    role decoretor

--- src/Decoretor/is-strong-password.decorator.ts
+++ src/Decoretor/is-strong-password.decorator.ts
@@ -1,59 +1,29 @@
 import {
-  registerDecorator,
-  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
   ValidationArguments,
 } from 'class-validator';
 
-export function IsStrongPassword(
-  validationOptions?: ValidationOptions,
-) {
-  return function (object: Object, propertyName: string) {
-    registerDecorator({
-      name: 'IsStrongPassword',
-      target: object.constructor,
-      propertyName,
-      options: validationOptions,
-      validator: {
-        validate(value: string) {
-          if (!value) return false;
+@ValidatorConstraint({ name: 'StrongPassword', async: false })
+export class StrongPassword implements ValidatorConstraintInterface {
+  validate(value: string) {
+    if (!value) return false;
 
-          const hasMinLength = value.length >= 8;
-          const hasTwoLowercase =
-            (value.match(/[a-z]/g) || []).length >= 2;
-          const hasUppercase = /[A-Z]/.test(value);
-          const hasNumber = /\d/.test(value);
-          const hasSpecialChar = /[@$!%*?&]/.test(value);
+    const errors: string[] = [];
 
-          return (
-            hasMinLength &&
-            hasTwoLowercase &&
-            hasUppercase &&
-            hasNumber &&
-            hasSpecialChar
-          );
-        },
+    if (value.length < 8) errors.push('Password must be at least 8 characters long');
+    if ((value.match(/[a-z]/g) || []).length < 2)
+      errors.push('Password must contain at least 2 lowercase letters');
+    if (!/[A-Z]/.test(value)) errors.push('Password must contain at least 1 uppercase letter');
+    if (!/\d/.test(value)) errors.push('Password must contain at least 1 number');
+    if (!/[@$!%*?&]/.test(value)) errors.push('Password must contain at least 1 special character');
 
-        defaultMessage(args: ValidationArguments) {
-          const value = args.value || '';
+    (this as any).errors = errors;
+    return errors.length === 0;
+  }
 
-          if (value.length < 8)
-            return 'Password must be at least 8 characters long';
-
-          if ((value.match(/[a-z]/g) || []).length < 2)
-            return 'Password must contain at least 2 lowercase letters';
-
-          if (!/[A-Z]/.test(value))
-            return 'Password must contain at least 1 uppercase letter';
-
-          if (!/\d/.test(value))
-            return 'Password must contain at least 1 number';
-
-          if (!/[@$!%*?&]/.test(value))
-            return 'Password must contain at least 1 special character';
-
-          return 'Password is not strong enough';
-        },
-      },
-    });
-  };
+  defaultMessage(args: ValidationArguments) {
+    const errors: string[] = (this as any).errors || [];
+    return errors.join('  &&  ');
+  }
 }
